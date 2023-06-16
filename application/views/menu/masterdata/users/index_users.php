b6e5668c71c05aeed50149554239ba78ef040bc6
Andrean <andreankun3@gmail.com>
2023-06-16 21:31:03 +0700

    Update

---
+++ application/views/menu/masterdata/users/index_users.php
@@ -0,0 +1,22 @@
+<div class="col-xl-12 mb-2">
+	<div class="modal-header" style="padding: 5px;">
+		<div class="col-xl-12">
+			<div class="row">
+				<div class="col-md-5">
+					<h2>
+						<span class="logo-menu"><?= $icons ?></span>
+						<span class="text-uppercase"><?= $title ?></span>
+					</h2>
+				</div>
+				<div class="col-md-7">
+					<div class="form-group" style="float: right;">
+						<button id="save" class="btn btn-sm btn-dark mb-1 mt-1 mr-1 ml-1" style="margin-bottom: 0px;"><i class="far fa-save" style="margin-right: 10px;"></i>Save</button>
+						<button id="update" class="btn btn-sm btn-dark mb-1 mt-1 mr-1 ml-1" style="margin-bottom: 0px;"><i class="far fa-money-check-edit" style="margin-right: 10px;"></i>Update</button>
+						<button data-toggle="modal" data-target="#finddisckkhususpelanggan" id="find" class="btn btn-sm btn-dark mb-1 mt-1 mr-1 ml-1" style="margin-bottom: 0px;"><i class="far fa-file-search" style="margin-right: 10px;"></i>Find</button>
+						<button id="clear" class="btn btn-sm btn-dark mb-1 mt-1 mr-1 ml-1" style="margin-bottom: 0px;"><i class="far fa-refresh" style="margin-right: 10px;"></i>Clear</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
